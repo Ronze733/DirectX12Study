dd49ec4f223a8b55f70936e209604292f7842a93
Ronze733 <forttrl20@gmail.com>
2024-06-23 22:57:08 +0900

    6/23 study
    Direct X 12 초기화 부분 작성

--- DirectX12_basic/DirectX12_basic/main.cpp
+++ DirectX12_basic/DirectX12_basic/main.cpp
@@ -4,15 +4,14 @@
 #include <iostream>
 #endif
 
+#include <vector>
+
 #include <d3d12.h>
 #include <dxgi1_6.h>
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 
-ID3D12Device* _dev = nullptr;
-IDXGIFactory6* _dxgiFactory = nullptr;
-IDXGISwapChain4* _swapchain = nullptr;
 
 //#define window_width 1280;
 //#define window_height 860;
@@ -45,6 +44,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	DebugOutputFormatString("Show window test.");
 	getchar();
 
+	int windowWidth = 1280, windowHeight = 860;
+
 	// 윈도우 생성 & 등록
 	WNDCLASSEX w = {};
 	w.cbSize = sizeof(WNDCLASSEX);
@@ -54,7 +55,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	RegisterClassEx(&w);	// 프로그램 클래스(윈도우 클래스를 os에 전달)
 
-	RECT wrc = { 0, 0, 1280, 860 };	// 윈도우 사이즈를 결정
+	RECT wrc = { 0, 0, windowWidth, windowHeight };	// 윈도우 사이즈를 결정
 
 	// 함수를 사용해서 윈도우 사이즈를 보정
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
@@ -73,6 +74,133 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		nullptr									// 추가 파라미터
 	);
 
+	ID3D12Device* _dev = nullptr;
+	IDXGIFactory6* _dxgiFactory = nullptr;
+	IDXGISwapChain4* _swapchain = nullptr;
+
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+	D3D_FEATURE_LEVEL featureLevel;
+
+	for (auto lv : levels) {
+		if (D3D12CreateDevice(nullptr, lv, IID_PPV_ARGS(&_dev)) == S_OK) {
+			featureLevel = lv;
+			break;
+		}
+	}
+
+	auto result = CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory));
+
+	// 어댑터 열거형
+	vector<IDXGIAdapter*> adapters;
+
+	// 여기서 특정 이름을 가진 아댑터 오브젝트가 삽입
+	IDXGIAdapter* tmpAdapter = nullptr;
+
+	for (int i = 0; _dxgiFactory->EnumAdapters(i, &tmpAdapter) != DXGI_ERROR_NOT_FOUND; ++i) {
+		adapters.push_back(tmpAdapter);
+	}
+
+	for (auto adpt : adapters) {
+		DXGI_ADAPTER_DESC adesc = {};
+		adpt->GetDesc(&adesc);	// 어댑터의 설명 오브젝트 취득
+
+		std::wstring strDesc = adesc.Description;
+
+		// 찾고 싶은 어댑터의 이름 확인
+		if (strDesc.find(L"NVIDIA") != std::string::npos) {
+			tmpAdapter = adpt;
+			break;
+		}
+	}
+
+	ID3D12CommandAllocator* _cmdAllocator = nullptr;
+	ID3D12GraphicsCommandList* _cmdList = nullptr;
+
+	result = _dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&_cmdAllocator));
+
+	/*
+	* 리스트 생성 확인
+	if (result == S_OK) {
+		cout << "OK!" << endl;
+	}
+	*/
+
+	result = _dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, _cmdAllocator, nullptr, IID_PPV_ARGS(&_cmdList));
+	
+	/*
+	* 얼로케이터 생성 확인
+	if (result == S_OK) {
+		cout << "OK!" << endl;
+	}
+	*/
+
+	ID3D12CommandQueue* _cmdQueue = nullptr;
+
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = {};
+
+	// 시간제한 없음
+	cmdQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+
+	// 어댑터를 하나만 사용할 시에는 0으로 하면 됨
+	cmdQueueDesc.NodeMask = 0;
+
+	// 우선순위는 특별히 정하지 않음
+	cmdQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+
+	// 커맨드 리스트와 맞추기
+	cmdQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+	// 큐 생성
+	result = _dev->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&_cmdQueue));
+
+	/*
+	* 큐 생성 확인
+	if (result == S_OK) {
+		cout << "OK!" << endl;
+	}
+	*/
+
+	DXGI_SWAP_CHAIN_DESC1 swapchainDesc = {};
+
+	swapchainDesc.Width = windowWidth;
+	swapchainDesc.Height = windowHeight;
+	swapchainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapchainDesc.Stereo = false;
+	swapchainDesc.SampleDesc.Count = 1;
+	swapchainDesc.SampleDesc.Quality = 0;
+	swapchainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapchainDesc.BufferCount = 2;
+
+	// 백버퍼는 크기 변경 가능
+	swapchainDesc.Scaling = DXGI_SCALING_STRETCH;
+
+	// 플립 후에는 빠르게 파기
+	swapchainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+
+	// 특별한 지정 없음
+	swapchainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+
+	// 윈도우 <-> 풀 스크린 변경 가능
+	swapchainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	result = _dxgiFactory->CreateSwapChainForHwnd(
+		_cmdQueue, hwnd, &swapchainDesc, nullptr, nullptr,
+		(IDXGISwapChain1**)&_swapchain);		// 원래는 QueryInterface등을 이용하여 IDXGISwapchain4*에 변환 체크를 하지만
+												// 여기서는 알기 쉽게 하기 위해 캐스팅으로 대응
+
+	/*
+	if (result == S_OK) {
+		cout << "OK!" << endl;
+	} 
+	*/
+
+
 	ShowWindow(hwnd, SW_SHOW);
 
 	MSG msg = {};
@@ -87,8 +215,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			break;
 		}
 	}
-
+	
 	UnregisterClass(w.lpszClassName, w.hInstance);
+	
 
 	return 0;
 }
